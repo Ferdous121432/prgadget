0d37214bd3d36a9c76956f32770086340a3c9623
Md Firdous Azam <firdousazam@iut-dhaka.edu>
2025-08-05 01:44:34 +0600

    prisma

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@/lib/generated/prisma";
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+// import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
 
-const neon = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaNeon(neon as any);
+// const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+// const adapter = new PrismaNeon(neon as any);
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({
